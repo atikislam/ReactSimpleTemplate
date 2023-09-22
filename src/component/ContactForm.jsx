5aa73ea919cd5bc233d33c941c64ddac9b3402cc
atikislam <atikislam834@gmail.com>
2023-09-22 23:10:03 +0600

    Simple Contact Form

--- src/component/ContactForm.jsx
+++ src/component/ContactForm.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 const ContactForm = () => {
+    const [fData,setfData]=useState({fName:"",lName:"",city:"",gender:"Male"});
+    const handleChange = (data_key,value) =>{
+        setfData((prevObj)=>({
+            ...prevObj,
+            [data_key]:value
+        }))
+    }
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(fData);
+    }
     return (
-        <div>
-            <input type="text" />
-            <button>Submit</button>
-        </div>
+        <>
+            <form onSubmit={handleSubmit}>
+                <input onChange={(e)=>handleChange("fName",e.target.value)} value={fData.fName} type="text" name="" id="" />
+                <input onChange={(e)=>handleChange("lName",e.target.value)} value={fData.lName} type="text" name="" id="" />
+                <select onChange={(e)=>handleChange("city",e.target.value)} value={fData.city}>
+                    <option value="">Choose City</option>
+                    <option value="Dhaka">Dhaka</option>
+                    <option value="Natore">Natore</option>
+                </select>
+                <input onChange={()=>handleChange("gender",'Male')}  checked={fData.gender=="Male"} type="radio" name="gender" />Male
+                <input onChange={()=>handleChange("gender",'Female')}  checked={fData.gender=="Female"} type="radio" name="gender" />Female
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+        </>
     );
 };
 
